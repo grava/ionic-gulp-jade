9010b2b64b2494893b8d1f9e69feff082682038e
gianluca gravina <gianluca.gravina@gmail.com>
2016-08-11 21:13:43 +0200

    added test, fixed pug

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
-	 assign = require('lodash.assign');
+    pug = require('gulp-pug'),
+    debug = require('gulp-debug'),
+    assign = require('lodash.assign'),
+    gulpWatch = require('gulp-watch');
 
 var defaultOptions = {
   src: 'app/**/*.jade',
   dest: 'www/build/',
-  jadeOptions: { 
+  pugOptions: {
     pretty: true,
     doctype: 'html'
   },
-  done: function (){},
-  onError: function(err) {
+  done: function () { },
+  onError: function (err) {
     console.error(err.message);
     this.emit('end'); // Don't kill watch tasks - https://github.com/gulpjs/gulp/issues/259
   }
 };
 
 
-module.exports = function(options) {
+module.exports = function (options) {
   options = assign(defaultOptions, options);
 
-return gulp.src(options.src)
-      .pipe(jade(options.jadeOptions))
-      .on('error', options.onError)
-      .on('end', options.done)
-      .pipe(gulp.dest(options.dest))
-      .on('end', options.done);
+  console.log(options.src);
+  console.log(options.dest);
+  return gulp.src(options.src)
+    .pipe(debug({title: "processing: "}))
+    .pipe(pug(options.pugOptions))
+    .on('error', options.onError)
+    .on('end', options.done)
+    .pipe(gulp.dest(options.dest))
+    .pipe(debug({title: "writing: "}))
+    .on('end', options.done);
 }
 
--- tests/gulpfile.js
+++ tests/gulpfile.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var gulp = require('gulp'),
-    buildJade = require('../index');
+    buildJade = require('../index'),
+    runSequence = require('run-sequence'),
+    gulpWatch = require('gulp-watch'),
+    del = require('del');
 
+gulp.task('watch', ['clean'], function(done){
+  runSequence(
+    ['pug'],
+    function(){
+      gulpWatch('./src/**/*.jade', function(){ gulp.start('pug'); });
+    }
+  );
+});
 
+gulp.task('clean', function() {
+  return del('./dest');
+})
    
-gulp.task('default', function() {
+gulp.task('pug', function() {
   return buildJade(
   {
     src: './src/**/*.jade',
-    dest: './dest',
-    jadeOptions: {
-      pretty: true,
-      doctype: 'html'
-    }
+    dest: './dest'
    });
 })
